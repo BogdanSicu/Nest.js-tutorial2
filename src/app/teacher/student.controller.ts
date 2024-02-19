927a759b89ee8a9c882bba0417fa2c03dbaf41a0
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2024-02-19 11:16:44 +0200

    added controllers

---
+++ src/app/teacher/student.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Put } from "@nestjs/common"
+
+@Controller('teachers/:teacherId/students')
+export class StudentTeacherController {
+
+    @Get()
+    getStudents() {
+        return "Get all students that belong to a teacher";
+    }
+
+    @Put('/:studentId')
+    updateStudentTeacher() {
+        return 'Update Student Teacher'
+    }
+}

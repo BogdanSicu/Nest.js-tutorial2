630e87eb018d577b22bf7dcf9b67fb781305ee22
Sicu Bogdan-Andrei <b0gdan.munt3@gmail.com>
2024-02-21 11:12:30 +0200

    broke de app because I changed the response types

--- src/app/teacher/student.controller.ts
+++ src/app/teacher/student.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get, Param, Put } from "@nestjs/common"
+import { FindStudentResponseDto, StudentResponseDto } from "../student/dto/student.dto";
 
 @Controller('teachers/:teacherId/students')
 export class StudentTeacherController {
@@ -6,7 +7,7 @@ export class StudentTeacherController {
     @Get()
     getStudents(
         @Param('teacherId') teacherId: string
-    ) {
+    ): FindStudentResponseDto[] {
         return `Get all students that belong to a teacher with id of ${teacherId}`;
     }
 
@@ -14,7 +15,7 @@ export class StudentTeacherController {
     updateStudentTeacher(
         @Param('teacherId') teacherId: string,
         @Param('studentId') studentId: string
-    ) {
+    ): StudentResponseDto {
         return `Update Student with id ${studentId} to Teacher with id ${teacherId}`;
     }
 }
